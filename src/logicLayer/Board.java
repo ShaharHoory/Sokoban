6a56c7752f34f1f35a6317d5d75e6c44b82dc2d0
ShaharHoory <shaharhoo@gmail.com>
2017-05-22 19:26:14 +0300

    board branch

--- src/logicLayer/Board.java
+++ src/logicLayer/Board.java
@@ -1,22 +1,30 @@
 package logicLayer;
 
-public class Board {
-	
+import java.awt.GridLayout;
+
+import javax.swing.JPanel;
+
+public class Board extends JPanel {
+
 	private Cell[][] _cells;
-	
-	
+
 	public Board(Cell[][] cells) {
+		super(new GridLayout(cells.length, cells[0].length)); // check if
+																// cells[0].length
+																// is ok
 		_cells = cells;
+		// TODO: generate a for loop that draws every cell from cells param
+		// (puts an image according to the cell's type
 	}
 
-	//returns true if (x,y) is in the game board
+	// returns true if (x,y) is in the game board
 	private boolean isInBoard(int x, int y) {
-		return (x>=0 && x<_cells.length) && (y>=0 && y<_cells[x].length);
+		return (x >= 0 && x < _cells.length) && (y >= 0 && y < _cells[x].length);
 	}
 
-	//returns the cell at (x,y) if exists, null otherwise
+	// returns the cell at (x,y) if exists, null otherwise
 	private Cell getCellAt(int x, int y) {
-		if (!isInBoard(x,y))
+		if (!isInBoard(x, y))
 			return null;
 		return _cells[x][y];
 	}
