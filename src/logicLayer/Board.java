6a321551d2ab8c8464587008fbd7ccf4b192008c
ShaharHoory <shaharhoo@gmail.com>
2017-05-22 17:40:51 +0300

    getCell & isInBoard

--- src/logicLayer/Board.java
+++ src/logicLayer/Board.java
@@ -4,5 +4,16 @@ public class Board {
 	
 	private Cell[][] _cells;
 	
+	//returns true if (x,y) is in the game board
+	private boolean isInBoard(int x, int y) {
+		return (x>=0 && x<_cells.length) && (y>=0 && y<_cells[x].length);
+	}
+
+	//returns the cell at (x,y) if exists, null otherwise
+	private Cell getCellAt(int x, int y) {
+		if (!isInBoard(x,y))
+			return null;
+		return _cells[x][y];
+	}
 
 }
