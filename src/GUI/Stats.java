950d8f9169104481a08da666cfcdbb6610d76da3
Tom Marzea <tommarz@post.bgu.ac.il>
2017-05-30 14:27:41 +0300

    added a popup menu

--- src/GUI/Stats.java
+++ src/GUI/Stats.java
@@ -1,30 +1,43 @@
 package GUI;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.TextArea;
 import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.*;
-import java.util.Timer;
 
 import javax.swing.*;
+import javax.swing.Timer;
 import logicLayer.*;
 
-public class Stats extends JPanel {
+public class Stats extends JPanel implements ActionListener  {
 
 	private JLabel _score;
+	private JLabel _time;
 	private int _points;
-	private Timer _timer;
+	private GameStopwatch _stopwatch;
 	private Stack<Board> _prevSteps;
 	
 	public Stats() {
 		super();
 		_points = 0;
 		_prevSteps = new Stack<Board>();
-		this._score = initializeScore();
-		this.add(_score);
-		_score.setHorizontalAlignment(FlowLayout.LEFT);
-		this._timer = new Timer();
+		this.setLayout(new FlowLayout());
+		_score = initializeScore();
+		//_score.setHorizontalAlignment(FlowLayout.LEFT);
+		this.add(_score, FlowLayout.LEFT);
+		_stopwatch = new GameStopwatch();
+		_time = initializeStopwatch();
+		this.add(_time);
+		//_time.setHorizontalAlignment(FlowLayout.RIGHT);
+		Timer t = new Timer(1000, this);
+		t.addActionListener(_stopwatch);
+		t.start();
+		this.setBackground(Color.WHITE);
 	}
 
 	public int get_score() {
@@ -41,8 +54,8 @@ public class Stats extends JPanel {
 		_score.setText("Score : " + _points);
 	}
 	
-	public Timer get_timer() {
-		return _timer;
+	public GameStopwatch get_stopwatch() {
+		return _stopwatch;
 	}
 	
 	private JLabel initializeScore() {
@@ -51,6 +64,12 @@ public class Stats extends JPanel {
 		score.setFont(new Font("Times New Roman", Font.BOLD, 18));	
 		return score;
 	}
+	
+	private JLabel initializeStopwatch() {
+		JLabel time = new JLabel("Time : " + _stopwatch.get_mins() + " Minutes , " + _stopwatch.get_secs() + " Seconds");
+		time.setFont(new Font("Times New Roman", Font.BOLD, 16));
+		return time;
+	}
 
 	public int get_points() {
 		return _points;
@@ -58,5 +77,10 @@ public class Stats extends JPanel {
 
 	public Stack<Board> get_prevSteps() {
 		return _prevSteps;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		_time.setText("Time : " + _stopwatch.get_mins() + " Minutes , " + _stopwatch.get_secs() + " Seconds");
 	}
 }
